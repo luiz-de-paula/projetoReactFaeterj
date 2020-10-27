39d33b02e6e7b89a687c8cf311edfd27144d01fc
Luiz-de-Paula <eletronica.do.lui@gmai.com>
2020-10-27 02:26:24 -0300

    primeiro commit da apicação.

---
+++ project-react-js/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+function App() {
+    return ( <div className ="App">
+        <header className = "App">
+            <div>
+                <h1>hello FAETERJ</h1>
+            
+            </div>
+            <h2>Estamos juntos</h2>
+        </header>     
+        </div>
+    );
+}
+
+export default App;
